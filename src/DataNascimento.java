252e80f02053c1b9ccc8e15399b7e459d0bcf4a2
Lise Fliess <lisefliess@gmail.com>
2022-11-09 16:28:27 -0300

    primeira e segunda atividade da aula  JAVA

---
+++ src/DataNascimento.java
@@ -0,0 +1,10 @@
+public class DataNascimento {
+    public static void main(String[] args) {
+        int dataNascimento = 1998;
+        int anoAtual = 2022;
+        int resultado = anoAtual - dataNascimento;
+
+        System.out.println("A pessoa tem ou vai completar" + resultado + "anos");
+
+    }
+}
