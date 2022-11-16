07bd4d820c03faa6c78f2fc6b1032fa88302aa1b
Lise Fliess <lisefliess@gmail.com>
2022-11-16 16:30:52 -0300

    primeiras Atividades condicionais

---
+++ src/farenheit.java
@@ -0,0 +1,11 @@
+public class farenheit {
+    public static void main(String[] args) {
+
+        double grauC = 30;
+        String grauF = "c";
+        double conversão = grauC * 1.8 + 32.0;
+
+        if (  )
+            System.out.println( );
+    }
+}
---
+++ src/notaAluno2.java
@@ -0,0 +1,15 @@
+public class notaAluno2 {
+    public static void main(String[] args) {
+
+        int nota = (2 + 4 + 3 + 1) / 4;
+        int media = nota;
+        System.out.println( " a sua média final é " + media);
+        if (media < 6) {
+
+            System.out.println("Você está reprovado");
+
+        } else {
+            System.out.println(" Você está Aprovado");
+        }
+    }
+}
