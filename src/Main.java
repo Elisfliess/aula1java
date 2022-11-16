2d9e5c03d766ff7adff004637ea5046ba4cf53c6
Lise Fliess <lisefliess@gmail.com>
2022-11-16 13:55:26 -0300

    Atividades concluida

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Lise Fliess");
+        System.out.println("Seja bem-vindo!");
+    }
+}
+
