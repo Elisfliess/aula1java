f4cee5786b5e025d0703c224df01f49fc9a67519
Lise Fliess <lisefliess@gmail.com>
2022-11-16 16:32:20 -0300

    primeiras Atividades condicionais

---
+++ src/condicionais.java
@@ -0,0 +1,13 @@
+public class condicionais {
+    public static void main(String[] args) {
+        boolean estaChovendo = false;
+        boolean estaMorno = true;
+
+
+        if (estaChovendo) {
+            System.out.println("Não vou sair");
+        } else {
+            System.out.println("vou sair");
+        }
+    }
+}
---
+++ src/notaAlunos.java
@@ -0,0 +1,19 @@
+public class notaAlunos {
+    public static void main(String[] args) {
+
+        int nota = (5 + 8 + 6 + 7) / 4;
+        int media = nota;
+
+        System.out.println("a sua média final é " + media);
+
+        if (media < 6) {
+
+            System.out.println("Você está reprovado !");
+
+        } else {
+            System.out.println(" Você está Aprovado! ");
+        }
+
+
+    }
+}
